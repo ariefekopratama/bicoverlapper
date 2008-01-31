146f88b33b3dd20bdaa08f5e29d53a0f9ec559a0
rodrigo.santamaria.vicente <rodrigo.santamaria.vicente@d85b0e2d-8643-0410-831e-d97f03b24863>
2008-01-31 17:10:50 +0000

    Initial import.

---
+++ BicOverlapper/es/usal/bicoverlapper/data/files/FileParser.java
@@ -0,0 +1,80 @@
+package es.usal.bicoverlapper.data.files;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * Singleton for file parsing utilities
+ * @author Rodrigo Santamaria
+ *
+ */
+public class FileParser {
+	private static final long serialVersionUID = 1898266950890009392L;
+
+	/**
+	 * Singleton instance
+	 */
+	public static FileParser instance = new FileParser();
+	
+	
+	private FileParser() {
+		// it's a singleton, and only supposed to be read from a file
+		}
+
+	
+	/**
+	 * Converts overlapper format for biclusters to bivoc format. 
+	 * Bivoc sofware and format is available at https://bioinformatics.cs.vt.edu/~ggrothau/BiVoC/
+	 * 
+	 * @param inPath	input path for Overlapper bicluster file
+	 * @param outPath	output path for Bivoc file
+	 */
+	public static void convertOverlapperToBivoc(String inPath, String outPath)
+		{
+		try{
+		BufferedReader in =	new BufferedReader(new FileReader(inPath));
+		BufferedWriter out =new BufferedWriter(new FileWriter(outPath));
+
+		String cad="";
+		int cont=0;
+		System.out.println("CONVERTER...");
+		while((cad=in.readLine())!=null)
+			{
+			String text[]=cad.split(" ");
+			if(text.length>1)
+				{
+				String entry="";
+				for(int i=0;i<3;i++)
+					{
+					switch(i)
+						{
+						case 0:	//name
+							text=cad.split(" ");
+							entry="bic"+cont+"_"+text[0]+"_"+text[1];
+							cont++;
+							break;
+						case 1:	//genes
+							cad=in.readLine();
+							text=cad.split(" ");
+							for(int j=0;j<text.length;j++)	entry=entry+"\t"+text[j];
+							break;
+						case 2:	//conditions
+							cad=in.readLine();
+							text=cad.split(" ");
+							for(int j=0;j<text.length;j++)	entry=entry+"\t"+text[j];
+							System.out.println("|"+entry+"|");
+							break;
+						}
+					}
+				out.write(entry);
+				out.newLine();
+				}
+			}
+		out.close();
+		}catch(Exception e){e.printStackTrace();}
+		System.out.println("Finished!");
+		}
+	
+	}
---
+++ BicOverlapper/es/usal/bicoverlapper/utils/LicensePanel.java
@@ -0,0 +1,67 @@
+package es.usal.bicoverlapper.utils;
+
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+
+public class LicensePanel extends JPanel
+	{
+
+    public JTextArea license;
+    public JLabel authors;
+    public JLabel group;
+    public JLabel lic;
+    
+    public LicensePanel()
+    	{
+    	super();
+    	authors=new JLabel("Design and development by Rodrigo Santamaria and Roberto Theron");
+    	group=new JLabel("      \tVISUSAL Group (http://vis.usal.es)");
+    	lic=new JLabel("This software is distributed under the MIT Licence:");
+    	license=new JTextArea();
+    	license.setText("The MIT License\n	Copyright (c) <2008> <Rodrigo Santamaría and Roberto Therón>\n\n" +
+    			" Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+    			" of this software and associated documentation files (the \"Software\"), to deal\n" +
+    					" in the Software without restriction, including without limitation the rights\n" +
+    					" to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+    					" copies of the Software, and to permit persons to whom the Software is\n" +
+    					" furnished to do so, subject to the following conditions:\n" +
+    					" \n" +
+    					"\n" +
+    					" The above copyright notice and this permission notice shall be included in\n" +
+    					" all copies or substantial portions of the Software.\n" +
+    					"\n" +
+    					"   THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+    							"   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+    							"   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+    							"   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+    							"   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+    							"   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" +
+    							"   THE SOFTWARE.");
+    	license.setSize(300,300);
+    	this.add(authors);
+    	this.add(group);
+    	this.add(lic);
+    	this.add(license);
+        //Create and set up the window.
+        JFrame frame = new JFrame("About BicOverlapper");
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+        //Create and set up the content pane.
+       // JComponent newContentPane = new ProgressBarDemo();
+        JComponent newContentPane = this;
+         newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.add(license);
+        frame.pack();
+        frame.setSize(550,400);
+        frame.setLocation(100,100);
+        
+        frame.setVisible(true);
+    	}
+ 
+	}
