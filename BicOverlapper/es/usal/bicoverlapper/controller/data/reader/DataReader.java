521dee0ae9636565810ba871111edff6c6d31770
carlosmartincasado <carlosmartincasado@gmail.com>
2012-02-07 23:32:51 +0000

    Trying to update.

--- BicOverlapper/es/usal/bicoverlapper/controller/data/reader/DataReader.java
+++ BicOverlapper/es/usal/bicoverlapper/controller/data/reader/DataReader.java
@@ -24,265 +24,300 @@ import es.usal.bicoverlapper.view.main.BicOverlapperWindow;
 import es.usal.bicoverlapper.view.main.WorkDesktop;
 
 /**
- * This class implements different methods to read data file formats required by BicOverlapper
+ * This class implements different methods to read data file formats required by
+ * BicOverlapper
+ * 
  * @author Rodrigo
- *
+ * 
  */
 public class DataReader {
-	private final int WINDOWS=0;
-	private final int LINUX=1;
+	private final int WINDOWS = 0;
+	private final int LINUX = 1;
 	private int OS;
-	
-	public DataReader()
-		{
-		if(System.getProperty("os.name").contains("indows"))	OS=WINDOWS;
-		else													OS=LINUX;//it includes macOSX, now with Linux paths
-		}
 
-	//----------------------------- FUNCIONES DE LECTURA MIAS ------------------------
+	public DataReader() {
+		if (System.getProperty("os.name").contains("indows"))
+			OS = WINDOWS;
+		else
+			OS = LINUX;// it includes macOSX, now with Linux paths
+	}
+
+	// ----------------------------- FUNCIONES DE LECTURA MIAS
+	// ------------------------
 	/**
 	 * Reads a microarray data file with the BicOverlapper format, that is:
-	 * organism/chip	col1	col2	...	colN
-	 * row1	exp11	exp12	...	exp1N
-	 * ...
-	 * rowM	expM1	expM2	...	expMN
+	 * organism/chip col1 col2 ... colN row1 exp11 exp12 ... exp1N ... rowM
+	 * expM1 expM2 ... expMN
 	 */
-	public void readMicroarray(String path,Session sesion, MicroarrayRequester mr) throws Exception 
-		{
-		int skipColumns=1;
-		int skipRows=1;
-		double t1=System.currentTimeMillis();
-		double t2=System.currentTimeMillis();
-		MicroarrayData md=new MicroarrayData(path, false, skipRows,skipColumns,1, mr, sesion.analysis);
+	public void readMicroarray(String path, Session sesion,
+			MicroarrayRequester mr) throws Exception {
+		int skipColumns = 1;
+		int skipRows = 1;
+		double t1 = System.currentTimeMillis();
+		double t2 = System.currentTimeMillis();
+		MicroarrayData md = new MicroarrayData(path, false, skipRows,
+				skipColumns, 1, mr, sesion.analysis);
 		sesion.setMicroarrayData(md);
-		t1=System.currentTimeMillis();
-		System.out.println("Time to load microarray data: "+(t1-t2)/1000+" seconds");
-		t2=System.currentTimeMillis();
-		System.out.println("Time to load fichero: "+(t2-t1)/1000+" seconds");
-		}
-	
+		t1 = System.currentTimeMillis();
+		System.out.println("Time to load microarray data: " + (t1 - t2) / 1000
+				+ " seconds");
+		t2 = System.currentTimeMillis();
+		System.out.println("Time to load fichero: " + (t2 - t1) / 1000
+				+ " seconds");
+	}
+
 	/**
 	 * Reads a Transcription Regulatory Network in GML or Syntren's XML format
 	 */
-	public void readTRN(String path, File fichero,Session sesion, String fileType) throws FileNotFoundException,IOException 
-		{
-		BufferedReader in =	new BufferedReader(new FileReader(fichero));
+	public void readTRN(String path, File fichero, Session sesion,
+			String fileType) throws FileNotFoundException, IOException {
+		BufferedReader in = new BufferedReader(new FileReader(fichero));
 		String variable = null;
-		BicOverlapperWindow window=sesion.mainWindow;
-		JDesktopPane desktop=sesion.getDesktop();
-		
-			
+		BicOverlapperWindow window = sesion.mainWindow;
+		JDesktopPane desktop = sesion.getDesktop();
+
 		variable = in.readLine();
-		//En función de esto determinamos qué es:
-		//1) Un fichero de tabla xml como los que usa Javi (usamos su método de lectura) a DatosFile
-		//2) Un grafo xml de los que usamos nosotros, usamos nuestro parser a TRN Data
-		//3) Un fichero txt de los que usamos para los resultados de biclustering, usamos
-		//	nuestro lector a BubbleData
-		BufferedReader in2=in; //Para no estropear lo que se lea de in
-		NetworkData trnd=null;
-		if(variable.contains("GeneNetwork"))
-			{
-			trnd=new NetworkData(getPath(path));//Syntren format		
+		// En función de esto determinamos qué es:
+		// 1) Un fichero de tabla xml como los que usa Javi (usamos su método de
+		// lectura) a DatosFile
+		// 2) Un grafo xml de los que usamos nosotros, usamos nuestro parser a
+		// TRN Data
+		// 3) Un fichero txt de los que usamos para los resultados de
+		// biclustering, usamos
+		// nuestro lector a BubbleData
+		BufferedReader in2 = in; // Para no estropear lo que se lea de in
+		NetworkData trnd = null;
+		if (variable.contains("GeneNetwork")) {
+			trnd = new NetworkData(getPath(path));// Syntren format
+		} else if (variable.contains("?xml")) {
+			String linea2 = in2.readLine();
+			if (linea2.contains("graphml")) {
+				trnd = new NetworkData(getPath(path));// GraphML
 			}
-		else if(variable.contains("?xml"))
-			{
-			String linea2=in2.readLine();
-			if(linea2.contains("graphml"))
-				{
-				trnd=new NetworkData(getPath(path));//GraphML			
-				}
-			}
-		else
-			{
-			trnd=new NetworkData(getPath(path));
-			}
-		
+		} else {
+			trnd = new NetworkData(getPath(path));
+		}
+
 		in.close();
 		in2.close();
-		if(sesion.getMicroarrayData()!=null)
-			{//Set ids equivalent to the session ids
-			for(int i=0;i<trnd.getGraph().getNodeCount();i++)
-				{
-				Node n=trnd.getGraph().getNode(i);
-				n.setInt("id", sesion.getMicroarrayData().getGeneId(n.getString("name").trim()));
-				}
+		if (sesion.getMicroarrayData() != null) {// Set ids equivalent to the
+													// session ids
+			for (int i = 0; i < trnd.getGraph().getNodeCount(); i++) {
+				Node n = trnd.getGraph().getNode(i);
+				n.setInt(
+						"id",
+						sesion.getMicroarrayData().getGeneId(
+								n.getString("name").trim()));
 			}
-		else
-			{
-			for(int i=0;i<trnd.getGraph().getNodeCount();i++)
-				{
-				Node n=trnd.getGraph().getNode(i);
-				n.setInt("id", i	);
-				}
+		} else {
+			for (int i = 0; i < trnd.getGraph().getNodeCount(); i++) {
+				Node n = trnd.getGraph().getNode(i);
+				n.setInt("id", i);
 			}
-		
-		boolean error=false;//TODO: check possible errors
-		if(!error){
+		}
+
+		boolean error = false;// TODO: check possible errors
+		if (!error) {
 			window.viewMenu.setEnabled(true);
 			window.menuViewTRN.setEnabled(true);
-			if(path!=null && path.length()>0)
-				{
-				try{
-					BufferedWriter pathWriter=new BufferedWriter(new FileWriter(sesion.reader.getPath("es/usal/bicoverlapper/data/networkPath.txt")));
+			if (path != null && path.length() > 0) {
+				try {
+					BufferedWriter pathWriter = new BufferedWriter(
+							new FileWriter(
+									sesion.reader
+											.getPath("es/usal/bicoverlapper/data/networkPath.txt")));
 					pathWriter.write(path);
 					pathWriter.close();
-					}catch(IOException ex){ex.printStackTrace();}
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
-				
-			if(window.getActiveWorkDesktop()==null)
-					window.addWorkDesktop(new WorkDesktop(desktop,sesion));
-			else			
-				{
-				JDesktopPane p=window.getActiveWorkDesktop().getPanel();
-				String title=window.getDesktop().getTitleAt(window.getDesktop().getSelectedIndex());
-				if(title.contains(".bic") || title.contains(".tmp"))
-					{
-					if(title.endsWith(".bic") || title.endsWith(".tmp"))
-						{
-						if(title.contains("|"))	title=title.substring(0, title.lastIndexOf("|")).trim();
-						else					title="";
-						}
+			}
+
+			if (window.getActiveWorkDesktop() == null)
+				window.addWorkDesktop(new WorkDesktop(desktop, sesion));
+			else {
+				JDesktopPane p = window.getActiveWorkDesktop().getPanel();
+
+				// Carlos
+				/*
+				 * String
+				 * title=window.getDesktop().getTitleAt(window.getDesktop(
+				 * ).getSelectedIndex()); if(title.contains(".bic") ||
+				 * title.contains(".tmp")) { if(title.endsWith(".bic") ||
+				 * title.endsWith(".tmp")) { if(title.contains("|"))
+				 * title=title.substring(0, title.lastIndexOf("|")).trim(); else
+				 * title=""; } } title=title+" | "+fichero.getName();
+				 * window.getDesktop
+				 * ().setTitleAt(window.getDesktop().getSelectedIndex(), title);
+				 * p.setName(title);
+				 */
+
+				// Carlos
+				String title = window.getTitle();
+				if (title.contains(".bic") || title.contains(".tmp")) {
+					if (title.endsWith(".bic") || title.endsWith(".tmp")) {
+						if (title.contains("|"))
+							title = title.substring(0, title.lastIndexOf("|"))
+									.trim();
+						else
+							title = "";
 					}
-				title=title+" | "+fichero.getName();
-				window.getDesktop().setTitleAt(window.getDesktop().getSelectedIndex(), title);
-				p.setName(title);
 				}
+				title = title + " | " + fichero.getName();
+				window.setTitle(title);
+				p.setName(title);
 			}
+		}
 		sesion.setTRNData(trnd);
+	}
+
+	// TODO: @deprecated
+	public String getPath(String path) {
+		String p = path;
+		switch (OS) {
+		case WINDOWS:
+			p = p.replace("/", "\\");
+			break;
+		case LINUX:
+			p = p.replace("\\", "/");
+			break;
 		}
-	
-	//TODO: @deprecated
-	public String getPath(String path)
-		{
-		String p=path;
-		switch(OS)
-			{
-			case WINDOWS:
-				p=p.replace("/","\\");
-				break;
-			case LINUX:
-				p=p.replace("\\","/");
-				break;
-			}
 		return p;
-		}
+	}
 
 	/**
 	 * Reads Biclustering Results in a BiMax format
+	 * 
 	 * @param path
 	 * @param fichero
 	 * @param sesion
 	 * @throws Exception
 	 */
-	public void readBiclusterResults(String path, String fichero,Session sesion) 
-		{
-		//readBiclusterResults(path, fichero, getPath(path+"\\"+fichero), sesion);
+	public void readBiclusterResults(String path, String fichero, Session sesion) {
+		// readBiclusterResults(path, fichero, getPath(path+"\\"+fichero),
+		// sesion);
 		readBiclusterResults(path, fichero, path, sesion);
-		}
-	
-	public void readBiclusterResults(String path, String fileName, String file, Session sesion)
-		{
-		BicOverlapperWindow window=sesion.mainWindow;
-		JDesktopPane desktop=sesion.getDesktop();
-		boolean error=false;
-		
-		try{
-			BubbleData bd=new BubbleData(file, sesion.getMicroarrayData()); 
+	}
+
+	public void readBiclusterResults(String path, String fileName, String file,
+			Session sesion) {
+		BicOverlapperWindow window = sesion.mainWindow;
+		JDesktopPane desktop = sesion.getDesktop();
+		boolean error = false;
+
+		try {
+			BubbleData bd = new BubbleData(file, sesion.getMicroarrayData());
 			bd.getGraph().getNodes().getClientProperty("name");
 			sesion.setBubbleData(bd);
-		
+
 			sesion.setBiclusterDataFile(file);
-			
-			sesion.setBubbleDataLoaded(true);//TODO para que sea true de verdad
+
+			sesion.setBubbleDataLoaded(true);// TODO para que sea true de verdad
 			sesion.setBiclusterDataStatus(true);
-			}
-		catch (FileNotFoundException e1) {
+		} catch (FileNotFoundException e1) {
+			JOptionPane.showMessageDialog(null, "File not found: " + file,
+					"Error", JOptionPane.ERROR_MESSAGE);
+			error = true;
+		} catch (IOException e2) {
+			JOptionPane.showMessageDialog(null, "I/O Error " + e2.getMessage(),
+					"Error", JOptionPane.ERROR_MESSAGE);
+			error = true;
+		} catch (Exception e3) {
 			JOptionPane.showMessageDialog(null,
-                    "File not found: "+file,
-                    "Error",JOptionPane.ERROR_MESSAGE);
-			error=true;
-			}
-		catch (IOException e2) 
-			{	
-			JOptionPane.showMessageDialog(null,
-	                "I/O Error "+e2.getMessage(),
-	                "Error",JOptionPane.ERROR_MESSAGE);
-			error=true;
-			}
-		catch (Exception e3) 
-			{
-			JOptionPane.showMessageDialog(null,
-	                "Format error "+e3.getMessage(),
-	                "Error",JOptionPane.ERROR_MESSAGE);
-			error=true;
-			}
-		if(!error)
-			{
+					"Format error " + e3.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+			error = true;
+		}
+		if (!error) {
 			window.viewMenu.setEnabled(true);
 			window.menuViewOverlapper.setEnabled(true);
 			window.menuViewBubbles.setEnabled(true);
-			if(path!=null && path.length()>0)
-				{
-				try{
-					BufferedWriter pathWriter=new BufferedWriter(new FileWriter(sesion.reader.getPath("es/usal/bicoverlapper/data/groupsPath.txt")));
+			if (path != null && path.length() > 0) {
+				try {
+					BufferedWriter pathWriter = new BufferedWriter(
+							new FileWriter(
+									sesion.reader
+											.getPath("es/usal/bicoverlapper/data/groupsPath.txt")));
 					pathWriter.write(path);
 					pathWriter.close();
-					}catch(IOException ex){ex.printStackTrace();}
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
-				
-			if(window.getActiveWorkDesktop()==null)
-					window.addWorkDesktop(new WorkDesktop(desktop,sesion));
-			else			
-				{
-				JDesktopPane p=window.getActiveWorkDesktop().getPanel();
-				
-				String title=window.getDesktop().getTitleAt(window.getDesktop().getSelectedIndex());
-				if(title.contains(".bic") || title.contains(".tmp"))
-					{
-					if(title.endsWith(".bic") || title.endsWith(".tmp"))
-						{
-						if(title.contains("|"))	title=title.substring(0, title.lastIndexOf("|")).trim();
-						else					title="";
-						}
+			}
+
+			if (window.getActiveWorkDesktop() == null)
+				window.addWorkDesktop(new WorkDesktop(desktop, sesion));
+			else {
+				JDesktopPane p = window.getActiveWorkDesktop().getPanel();
+
+				// Carlos
+				/*
+				 * String
+				 * title=window.getDesktop().getTitleAt(window.getDesktop(
+				 * ).getSelectedIndex()); if(title.contains(".bic") ||
+				 * title.contains(".tmp")) { if(title.endsWith(".bic") ||
+				 * title.endsWith(".tmp")) { if(title.contains("|"))
+				 * title=title.substring(0, title.lastIndexOf("|")).trim(); else
+				 * title=""; } } title=title+" | "+fileName;
+				 * //window.getDesktop().setTitleAt(0,
+				 * p.getName()+" | "+fileName);
+				 * //p.setName(p.getName()+" | "+fileName);
+				 * window.getDesktop().setTitleAt
+				 * (window.getDesktop().getSelectedIndex(), title);
+				 * p.setName(title);
+				 */
+
+				// Carlos
+				String title = window.getTitle();
+				if (title.contains(".bic") || title.contains(".tmp")) {
+					if (title.endsWith(".bic") || title.endsWith(".tmp")) {
+						if (title.contains("|"))
+							title = title.substring(0, title.lastIndexOf("|"))
+									.trim();
+						else
+							title = "";
 					}
-				title=title+" | "+fileName;
-				//window.getDesktop().setTitleAt(0, p.getName()+" | "+fileName);
-				//p.setName(p.getName()+" | "+fileName);
-				window.getDesktop().setTitleAt(window.getDesktop().getSelectedIndex(), title);
-				p.setName(title);
 				}
+				title = title + " | " + fileName;
+				window.setTitle(title);
+				p.setName(title);
+
+				// Carlos
+				// window.setTitle(window.getTitle() + " | " + fileName);
+
 			}
 		}
-	
+	}
 	/**
-	 * Metodo que nos parsea el parametro <code>variable</code> para obtener los datos contenidos y 
-	 * devolverlos en el parametro <code>vect</code>. Ademas nos devuelve un boolean, cuando dicho boolean es <code>true</code> nos
-	 * indica que <code>variable</code> contenia los datos de una variable, en caso de que nos devuelva <code>false</code> nos
-	 * indica que <code>variable</code> contenia las etiquetas de las tuplas formadas por las variables.
+	 * Metodo que nos parsea el parametro <code>variable</code> para obtener los
+	 * datos contenidos y devolverlos en el parametro <code>vect</code>. Ademas
+	 * nos devuelve un boolean, cuando dicho boolean es <code>true</code> nos
+	 * indica que <code>variable</code> contenia los datos de una variable, en
+	 * caso de que nos devuelva <code>false</code> nos indica que
+	 * <code>variable</code> contenia las etiquetas de las tuplas formadas por
+	 * las variables.
 	 * 
-	 * @param variable <code>String</code> que va a ser parseado.
-	 * @param vect Vector de <code>String</code> que contiene el parseo de <code>variable</code>.
-	 * @return Devuelve un <code>true</code> si el parseo produce una variable, y devuelve <code>false</code> si el parseo
-	 * produce las etiquetas de las tuplas. 
-	 *//*
-	private boolean isVar(String variable, Vector<String> vect) {
-		StringTokenizer tokens = new StringTokenizer(variable,",");
-		boolean isVar = true;
-		
-		vect.addElement(tokens.nextToken());
-		
-		while(tokens.hasMoreTokens()){
-			
-			String valor = tokens.nextToken();
-			try{
-				Double.valueOf(valor);
-			}
-			catch (NumberFormatException e){
-				isVar = false;
-			}
-			vect.addElement(valor);
-		}		
-		return isVar;
-	}*/
+	 * @param variable
+	 *            <code>String</code> que va a ser parseado.
+	 * @param vect
+	 *            Vector de <code>String</code> que contiene el parseo de
+	 *            <code>variable</code>.
+	 * @return Devuelve un <code>true</code> si el parseo produce una variable,
+	 *         y devuelve <code>false</code> si el parseo produce las etiquetas
+	 *         de las tuplas.
+	 */
+	/*
+	 * private boolean isVar(String variable, Vector<String> vect) {
+	 * StringTokenizer tokens = new StringTokenizer(variable,","); boolean isVar
+	 * = true;
+	 * 
+	 * vect.addElement(tokens.nextToken());
+	 * 
+	 * while(tokens.hasMoreTokens()){
+	 * 
+	 * String valor = tokens.nextToken(); try{ Double.valueOf(valor); } catch
+	 * (NumberFormatException e){ isVar = false; } vect.addElement(valor); }
+	 * return isVar; }
+	 */
 
 }
